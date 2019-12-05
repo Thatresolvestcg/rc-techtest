95bd0e652c6b4ccd3e7252327202a51fc48a0123
thatresolvestcg <thatresolvestcg@gmail.com>
2019-12-05 20:18:53 +0000

    started styling

--- src/component/App.js
+++ src/component/App.js
@@ -3,13 +3,15 @@ import '../styles/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Where are you going?</h1>
-      <p>Pickup Location</p>
-      <input type='text' placeholder='City, Airport, Region, District' className='searchBox'>
-      </input>
-      <button type='button'>Search</button>
-    </div>
+    <span className='totalBox'>
+      <div className="App">
+        <h1>Where are you going?</h1>
+        <p className='pickUp'>Pickup Location</p>
+        <input type='text' placeholder='City, Airport, Region, District' className='searchBox'>
+        </input>
+        <button type='button' className='searchButton'>Search</button>
+      </div>
+    </span>
   );
 }
 
